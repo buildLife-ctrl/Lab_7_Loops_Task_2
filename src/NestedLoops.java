162038e2c6e54de55979be83961f841736b306b1
617998 <sinchuri.617998@swl.k12.oh.us>
2024-10-16 08:34:57 -0400

    2ini

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,5 +1,30 @@
-public class Main {
+import java.util.Scanner;
+
+public class NestedLoops {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //Prints asterisk as there is more of each in the next lines for 5 lines
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //goes opposite of the previous
+        for (int i = 5; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //prints 5 asterisk for 5 lines
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
